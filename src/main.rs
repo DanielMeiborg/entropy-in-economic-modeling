3158694df437369cf3e2bb11e4481d8e0a7ccabe
danielmeiborg <mail@danielmeiborg.com>
2022-11-21 21:23:35 +0100

    Change to crawl based states (se future docs) and implement Simulation::new()

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,84 @@
+use std::collections::HashMap;
+
 mod simulation;
+use simulation::{Action, Entity, Resource, Rule, Simulation, State};
 
 fn main() {
+    let resources = vec![Resource {
+        name: "money".to_string(),
+        description: "In millions of dollars".to_string(),
+        capacity: 100,
+        must_be_held: true,
+        per_entity: false,
+        max_held: 100,
+    }];
 
+    let initial_state = Box::new(simulation::State {
+        entities: vec![
+            Entity {
+                name: "A".to_string(),
+                resources: HashMap::from([(String::from("money"), 5)]),
+            },
+            Entity {
+                name: "B".to_string(),
+                resources: HashMap::from([(String::from("money"), 30)]),
+            },
+            Entity {
+                name: "C".to_string(),
+                resources: HashMap::from([(String::from("money"), 65)]),
+            },
+        ],
+        probability: 1.0,
+    });
+
+    let rules = vec![Box::new(Rule {
+        name: "Socialism".to_string(),
+        description: "Richer one gives to poorer one".to_string(),
+        condition: |state: &State| {
+            let mut money = Vec::new();
+            for entity in &state.entities {
+                for (resource, amount) in &entity.resources {
+                    if resource == "money" {
+                        money.push(amount);
+                    }
+                }
+            }
+            !money.iter().all(|&x| x == money[0])
+        },
+        probability: 0.5,
+        actions: |state: &State| {
+            let mut money = Vec::new();
+            for entity in &state.entities {
+                for (resource, amount) in &entity.resources {
+                    if resource == "money" {
+                        money.push(amount);
+                    }
+                }
+            }
+            let max_index = money
+                .iter()
+                .position(|&x| x == *money.iter().max().unwrap())
+                .unwrap();
+            let min_index = money
+                .iter()
+                .position(|&x| x == *money.iter().min().unwrap())
+                .unwrap();
+
+            vec![
+                Action {
+                    resource: "money".to_string(),
+                    entities: vec![state.entities[min_index].name.clone()],
+                    amount: 1,
+                },
+                Action {
+                    resource: "money".to_string(),
+                    entities: vec![state.entities[max_index].name.clone()],
+                    amount: -1,
+                },
+            ]
+        },
+    })];
+
+    let main = Simulation::new(resources, initial_state, rules);
+    println!("{}", main.entropy);
 }
--- src/simulation.rs
+++ src/simulation.rs
@@ -1,33 +1,76 @@
-use uuid::Uuid;
+use std::collections::HashMap;
 
-#[derive(PartialEq)]
-pub enum Capacity {
-    Tag,
-    Count(u64),
-}
-
-#[derive(PartialEq)]
+#[derive(PartialEq, Clone, Debug)]
 pub struct Entity {
     pub name: String,
-    pub resources: Vec<Capacity>,
+    pub resources: HashMap<String, u64>,
 }
 
+#[derive(PartialEq, Clone, Debug)]
 pub struct Resource {
     pub name: String,
-    pub capacity: Capacity,
-    pub limited: bool,
+    pub description: String,
+    pub capacity: u64,
     pub must_be_held: bool,
-    pub can_hold_multiple: bool,
+    pub per_entity: bool,
+    pub max_held: i64,
 }
 
-#[derive(PartialEq)]
+#[derive(PartialEq, Clone, Debug)]
 pub struct State {
     pub entities: Vec<Entity>,
+    pub probability: f64,
 }
 
+#[derive(Clone, Debug)]
+pub struct Action {
+    pub resource: String,
+    pub entities: Vec<String>,
+    pub amount: i64,
+}
+
+#[derive(Clone)]
+pub struct Rule {
+    pub name: String,
+    pub description: String,
+    pub condition: fn(&State) -> bool,
+    pub probability: f64,
+    pub actions: fn(&State) -> Vec<Action>,
+}
+
+#[derive(Clone)]
 pub struct Simulation {
     pub resources: Vec<Resource>,
     pub initial_state: Box<State>,
-    pub current_state: Box<State>,
+    pub possible_states: Vec<Box<State>>,
+    pub rules: Vec<Box<Rule>>,
     pub current_time: u64,
+    pub entropy: f64,
+}
+
+impl Simulation {
+    // TODO: Implement checks for input parameters
+    pub fn new(
+        resources: Vec<Resource>,
+        initial_state: Box<State>,
+        rules: Vec<Box<Rule>>,
+    ) -> Simulation {
+        Simulation {
+            resources: resources.clone(),
+            initial_state: initial_state.clone(),
+            rules: rules.clone(),
+            possible_states: vec![initial_state.clone()],
+            current_time: 0,
+            entropy: Simulation::get_entropy(&vec![initial_state], 0),
+        }
+    }
+
+    fn get_entropy(possible_states: &Vec<Box<State>>, t: u64) -> f64 {
+        let mut entropy: f64 = 0.0;
+        for state in possible_states {
+            entropy += state.probability * -state.probability.log2();
+        }
+
+        return entropy;
+    }
 }
