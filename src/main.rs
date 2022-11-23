6aaa256ea8a39c82be58c8f42820468cfc09d8ee
danielmeiborg <mail@danielmeiborg.com>
2022-11-23 16:00:49 +0100

    Add get_next_reachable_states

--- src/main.rs
+++ src/main.rs
@@ -1,84 +1,140 @@
 use std::collections::HashMap;
+use rayon::prelude::*;
 
 mod simulation;
-use simulation::{Action, Entity, Resource, Rule, Simulation, State};
+use simulation::*;
 
 fn main() {
     let resources = vec![Resource {
         name: "money".to_string(),
-        description: "In millions of dollars".to_string(),
-        capacity: 100,
-        must_be_held: true,
-        per_entity: false,
-        max_held: 100,
+        description: "In dollars".to_string(),
+        capacity: Capacity::Limited(Amount::Float(30.)),
+        capacity_per_entity: CapacityPerEntity::Unlimited,
     }];
 
-    let initial_state = Box::new(simulation::State {
-        entities: vec![
-            Entity {
-                name: "A".to_string(),
-                resources: HashMap::from([(String::from("money"), 5)]),
-            },
-            Entity {
-                name: "B".to_string(),
-                resources: HashMap::from([(String::from("money"), 30)]),
-            },
-            Entity {
-                name: "C".to_string(),
-                resources: HashMap::from([(String::from("money"), 65)]),
-            },
-        ],
-        probability: 1.0,
-    });
+    let initial_state = vec![
+        Entity {
+            name: "A".to_string(),
+            resources: HashMap::from([("money".to_string(), Amount::Float(1.))]),
+        },
+        Entity {
+            name: "B".to_string(),
+            resources: HashMap::from([("money".to_string(), Amount::Float(3.))]),
+        },
+        Entity {
+            name: "C".to_string(),
+            resources: HashMap::from([("money".to_string(), Amount::Float(6.))]),
+        },
+    ];
 
-    let rules = vec![Box::new(Rule {
-        name: "Socialism".to_string(),
-        description: "Richer one gives to poorer one".to_string(),
-        condition: |state: &State| {
-            let mut money = Vec::new();
-            for entity in &state.entities {
-                for (resource, amount) in &entity.resources {
-                    if resource == "money" {
-                        money.push(amount);
+    let rules = vec![
+        Box::new(Rule {
+            name: "Socialism".to_string(),
+            description: "Richer one gives 2 dollars to poorer one".to_string(),
+            condition: |state: &State| {
+                let mut money = Vec::new();
+                for entity in &state.entities {
+                    money.push(entity.resources.get("money").unwrap());
+                }
+                return money.par_iter().any(|&x| x == money[0]);
+            },
+            probability: 0.3,
+            actions: |state: &State| {
+                let mut money: Vec<Amount> = Vec::new();
+                for entity in &state.entities {
+                    money.push(entity.resources.get("money").unwrap().clone());
+                }
+                let max_index =  money
+                    .par_iter()
+                    .position_any(|x| *x == *money.par_iter().max().unwrap())
+                    .unwrap();
+                let min_index = money
+                    .par_iter()
+                    .position_any(|x| *x == *money.par_iter().min().unwrap())
+                    .unwrap();
+
+                let current_amount = state.entities[max_index].resources.get("money").unwrap();
+
+                vec![
+                    Action {
+                        name: "Get".to_string(),
+                        resource: "money".to_string(),
+                        entities: vec![state.entities[min_index].name.clone()],
+                        new_amount: current_amount.clone() + Amount::Float(1.),
+                    },
+                    Action {
+                        name: "Give".to_string(),
+                        resource: "money".to_string(),
+                        entities: vec![state.entities[max_index].name.clone()],
+                        new_amount: current_amount.clone() - Amount::Float(1.),
+                    },
+                ]
+            },
+        }),
+        Box::new(Rule {
+            name: "Capitalism".to_string(),
+            description: "If somebody has 4 or more dollar, they double their wealth".to_string(),
+            condition: |state: &State| {
+                for entity in &state.entities {
+                    let money = entity.resources.get("money").unwrap();
+                    if money >= &Amount::Float(4.) {
+                        return true;
                     }
                 }
-            }
-            !money.iter().all(|&x| x == money[0])
-        },
-        probability: 0.5,
-        actions: |state: &State| {
-            let mut money = Vec::new();
-            for entity in &state.entities {
-                for (resource, amount) in &entity.resources {
-                    if resource == "money" {
-                        money.push(amount);
+                return false;
+            },
+            probability: 0.5,
+            actions: |state| {
+                let mut actions = Vec::new();
+                for entity in &state.entities {
+                    let money = entity.resources.get("money").unwrap();
+                    if money >= &Amount::Float(4.) {
+                        actions.push(Action {
+                            name: "Get".to_string(),
+                            resource: "money".to_string(),
+                            entities: vec![entity.name.clone()],
+                            new_amount: money.clone() * Amount::Float(2.),
+                        });
                     }
                 }
+                return actions;
+            },
+        }),
+    ];
+
+    let mut main = Simulation::new(resources, initial_state, rules);
+    let mut entropies: Vec<f64> = Vec::new();
+    for t in 1..5 {
+        main.next_step();
+        println!("Time: {}", t);
+        println!("Entropy: {:?}", main.entropy);
+        println!(
+            "Number of reachable states: {:?}",
+            main.reachable_states.len()
+        );
+        entropies.push(main.entropy);
+        println!("Reachable states:");
+        for state in &main.reachable_states {
+            println!(
+                "  State {:?}, probability {:?}",
+                state.hash, state.probability
+            );
+            for entity in &state.entities {
+                println!("    Entity {:?}", entity.name);
+                for (resource, amount) in &entity.resources {
+                    println!("      Resource {:?}: {:?}", resource, amount);
+                }
             }
-            let max_index = money
-                .iter()
-                .position(|&x| x == *money.iter().max().unwrap())
-                .unwrap();
-            let min_index = money
-                .iter()
-                .position(|&x| x == *money.iter().min().unwrap())
-                .unwrap();
-
-            vec![
-                Action {
-                    resource: "money".to_string(),
-                    entities: vec![state.entities[min_index].name.clone()],
-                    amount: 1,
-                },
-                Action {
-                    resource: "money".to_string(),
-                    entities: vec![state.entities[max_index].name.clone()],
-                    amount: -1,
-                },
-            ]
-        },
-    })];
-
-    let main = Simulation::new(resources, initial_state, rules);
-    println!("{}", main.entropy);
+            println!("");
+        }
+        println!("================================================");
+    }
+    println!("Entropies: {:?}", entropies);
+    println!(
+        "Ratio of each entropy to the previous one: {:?}",
+        entropies
+            .windows(2)
+            .map(|w| w[1] / w[0])
+            .collect::<Vec<f64>>()
+    );
 }
--- src/simulation.rs
+++ src/simulation.rs
@@ -1,32 +1,112 @@
-use std::collections::HashMap;
+use std::cmp::Ordering;
+use std::collections::{hash_map::DefaultHasher, HashMap};
+use std::hash::{Hash, Hasher};
+use std::ops::{Add, Mul, Sub};
+use rayon::prelude::*;
+
+// TODO: construct an actual graph from the states and the connecting rules
+
+#[derive(PartialEq, PartialOrd, Clone, Debug)]
+pub enum Amount {
+    Integer(u64),
+    Float(f64),
+}
+
+impl Eq for Amount {}
+
+impl Ord for Amount {
+    fn cmp(&self, other: &Self) -> Ordering {
+        match (self, other) {
+            (Amount::Integer(a), Amount::Integer(b)) => a.cmp(b),
+            (Amount::Float(a), Amount::Float(b)) => a.partial_cmp(b).unwrap(),
+            (Amount::Integer(a), Amount::Float(b)) => (*a as f64).partial_cmp(b).unwrap(),
+            (Amount::Float(a), Amount::Integer(b)) => a.partial_cmp(&(*b as f64)).unwrap(),
+        }
+    }
+}
+
+impl Hash for Amount {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        match self {
+            Amount::Integer(x) => x.hash(state),
+            Amount::Float(x) => x.to_bits().hash(state),
+        }
+    }
+}
+
+impl Add for Amount {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        match (self, other) {
+            (Amount::Integer(a), Amount::Integer(b)) => Amount::Integer(a + b),
+            (Amount::Float(a), Amount::Float(b)) => Amount::Float(a + b),
+            (Amount::Integer(_), Amount::Float(_)) => panic!("Cannot add integer to float"),
+            (Amount::Float(_), Amount::Integer(_)) => panic!("Cannot add float to integer"),
+        }
+    }
+}
+
+impl Mul for Amount {
+    type Output = Self;
+
+    fn mul(self, other: Self) -> Self {
+        match (self, other) {
+            (Amount::Integer(a), Amount::Integer(b)) => Amount::Integer(a * b),
+            (Amount::Float(a), Amount::Float(b)) => Amount::Float(a * b),
+            (Amount::Integer(_), Amount::Float(_)) => panic!("Cannot multiply integer by float"),
+            (Amount::Float(_), Amount::Integer(_)) => panic!("Cannot multiply float by integer"),
+        }
+    }
+}
+
+impl Sub for Amount {
+    type Output = Self;
+
+    fn sub(self, other: Self) -> Self {
+        match (self, other) {
+            (Amount::Integer(a), Amount::Integer(b)) => Amount::Integer(a - b),
+            (Amount::Float(a), Amount::Float(b)) => Amount::Float(a - b),
+            (Amount::Integer(_), Amount::Float(_)) => panic!("Cannot subtract integer from float"),
+            (Amount::Float(_), Amount::Integer(_)) => panic!("Cannot subtract float from integer"),
+        }
+    }
+}
 
 #[derive(PartialEq, Clone, Debug)]
 pub struct Entity {
     pub name: String,
-    pub resources: HashMap<String, u64>,
+    pub resources: HashMap<String, Amount>,
 }
 
-#[derive(PartialEq, Clone, Debug)]
-pub struct Resource {
-    pub name: String,
-    pub description: String,
-    pub capacity: u64,
-    pub must_be_held: bool,
-    pub per_entity: bool,
-    pub max_held: i64,
+impl Hash for Entity {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.name.hash(state);
+        for pair in &self.resources {
+            pair.hash(state);
+        }
+    }
 }
 
 #[derive(PartialEq, Clone, Debug)]
 pub struct State {
     pub entities: Vec<Entity>,
+    pub hash: u64,
     pub probability: f64,
 }
 
-#[derive(Clone, Debug)]
+#[derive(PartialEq, Clone, Debug)]
+pub enum Number {
+    Integer(i64),
+    Float(f64),
+}
+
+#[derive(PartialEq, Clone, Debug)]
 pub struct Action {
+    pub name: String,
     pub resource: String,
     pub entities: Vec<String>,
-    pub amount: i64,
+    pub new_amount: Amount,
 }
 
 #[derive(Clone)]
@@ -38,11 +118,37 @@ pub struct Rule {
     pub actions: fn(&State) -> Vec<Action>,
 }
 
+#[derive(PartialEq, Clone, Debug, Hash)]
+pub enum UnlimitedCapacity {
+    Integer,
+    Float,
+}
+
+#[derive(PartialEq, Clone, Debug, Hash)]
+pub enum Capacity {
+    Limited(Amount),
+    Unlimited(UnlimitedCapacity),
+}
+
+#[derive(PartialEq, Clone, Debug, Hash)]
+pub enum CapacityPerEntity {
+    Limited(Amount),
+    Unlimited,
+}
+
+#[derive(PartialEq, Clone, Debug, Hash)]
+pub struct Resource {
+    pub name: String,
+    pub description: String,
+    pub capacity: Capacity,
+    pub capacity_per_entity: CapacityPerEntity,
+}
+
 #[derive(Clone)]
 pub struct Simulation {
     pub resources: Vec<Resource>,
     pub initial_state: Box<State>,
-    pub possible_states: Vec<Box<State>>,
+    pub reachable_states: Vec<Box<State>>,
     pub rules: Vec<Box<Rule>>,
     pub current_time: u64,
     pub entropy: f64,
@@ -52,22 +158,151 @@ impl Simulation {
     // TODO: Implement checks for input parameters
     pub fn new(
         resources: Vec<Resource>,
-        initial_state: Box<State>,
+        entities: Vec<Entity>,
         rules: Vec<Box<Rule>>,
     ) -> Simulation {
+        let mut hasher = DefaultHasher::new();
+        entities.hash(&mut hasher);
+        let initial_state = Box::new(State {
+            entities,
+            hash: hasher.finish(),
+            probability: 1.0,
+        });
         Simulation {
             resources: resources.clone(),
             initial_state: initial_state.clone(),
+            reachable_states: vec![initial_state.clone()],
             rules: rules.clone(),
-            possible_states: vec![initial_state.clone()],
             current_time: 0,
-            entropy: Simulation::get_entropy(&vec![initial_state], 0),
+            entropy: 0.,
         }
     }
 
-    fn get_entropy(possible_states: &Vec<Box<State>>, t: u64) -> f64 {
+    pub fn next_step(&mut self) {
+        self.reachable_states = self.get_next_reachable_states();
+        self.entropy = self.get_entropy();
+        self.current_time += 1;
+    }
+
+    fn append_reachable_state(
+        mut new_state: Box<State>,
+        next_reachable_states: &mut Vec<Box<State>>,
+    ) {
+        let mut hasher = DefaultHasher::new();
+        new_state.entities.hash(&mut hasher);
+        new_state.hash = hasher.finish();
+        match next_reachable_states
+            .par_iter()
+            .position_any(|x| x.hash == new_state.hash)
+        {
+            Some(index) => {
+                next_reachable_states[index].probability += new_state.probability;
+            }
+            None => {
+                next_reachable_states.push(new_state);
+            }
+        }
+    }
+
+    fn check_resources(&self, new_state: &Box<State>) {
+        for resource in &self.resources {
+            match &resource.capacity {
+                Capacity::Limited(amount) => match amount {
+                    Amount::Integer(limit) => {
+                        let mut total_amount: u64 = 0;
+                        for entity in &new_state.entities {
+                            let entity_amount =
+                                entity.resources.get(&resource.name).unwrap();
+                            match entity_amount {
+                                Amount::Integer(amount) => {
+                                    total_amount += amount;
+                                }
+                                Amount::Float(_) => {
+                                    panic!("Cannot combine Float amount with Integer resource in entity");
+                                }
+                            }
+                            if total_amount > *limit {
+                                panic!("Resource limit exceeded for resource {resource_name}",
+                                 resource_name = resource.name);
+                            }
+                        }
+                    }
+                    Amount::Float(limit) => {
+                        let mut total_amount: f64 = 0.;
+                        for entity in &new_state.entities {
+                            let entity_amount =
+                                entity.resources.get(&resource.name).unwrap();
+                            match entity_amount {
+                                Amount::Integer(_) => {
+                                    panic!("Cannot combine Integer amount with Float resource in entity");
+                                }
+                                Amount::Float(amount) => {
+                                    total_amount += amount;
+                                }
+                            }
+                            if total_amount > *limit {
+                                panic!("Resource limit exceeded for resource {resource_name}",
+                                 resource_name = resource.name);
+                            }
+                        }
+                    }
+                },
+                Capacity::Unlimited(_) => continue,
+            }
+        }
+    }
+
+    // TODO: implement caching
+    fn get_next_reachable_states(&self) -> Vec<Box<State>> {
+        let mut next_reachable_states: Vec<Box<State>> = Vec::new();
+        for state in &self.reachable_states {
+            let mut new_p_base_state = state.probability.clone();
+            for rule in &self.rules {
+                if (rule.condition)(state) && rule.probability > 0. {
+                    new_p_base_state = new_p_base_state * (1. - rule.probability);
+                    let mut new_state = state.clone();
+                    new_state.probability = rule.probability * state.probability;
+                    let actions = (rule.actions)(state);
+                    for action in actions {
+                        for entity_name in action.entities {
+                            let mut entity = new_state
+                                .entities
+                                .par_iter()
+                                .find_any(|x| x.name == entity_name)
+                                .unwrap()
+                                .clone();
+
+                            let capacity_per_entity = &self
+                                .resources
+                                .iter()
+                                .find(|&x| x.name == action.resource)
+                                .unwrap()
+                                .capacity_per_entity;
+
+                            // TODO: implement maximum capacity per entity
+                            entity
+                                .resources
+                                .insert(action.resource.clone(), action.new_amount.clone());
+                        }
+                    }
+
+                    self.check_resources(&new_state);
+                    Simulation::append_reachable_state(new_state, &mut next_reachable_states);
+                }
+            }
+            if new_p_base_state > 0. {
+                let mut new_base_state = state.clone();
+                new_base_state.probability = new_p_base_state;
+                Simulation::append_reachable_state(new_base_state, &mut next_reachable_states);
+            }
+        }
+
+        return next_reachable_states;
+    }
+
+    fn get_entropy(&self) -> f64 {
         let mut entropy: f64 = 0.0;
-        for state in possible_states {
+        for state in &self.reachable_states {
             entropy += state.probability * -state.probability.log2();
         }
 
